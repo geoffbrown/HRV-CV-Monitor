cfc2bea10e131805d1809c851692ebf2008a96e2
Geoffrey Brown <geoffreywbrown@gmail.com>
2026-07-18 11:01:17 -0700

    Initial Commit

---
+++ HRV-CV Monitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HRV-CV Monitor
+//
+//  Created by Geoffrey Brown on 7/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HRV-CV Monitor/HRV_CV_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HRV_CV_MonitorApp.swift
+//  HRV-CV Monitor
+//
+//  Created by Geoffrey Brown on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct HRV_CV_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
